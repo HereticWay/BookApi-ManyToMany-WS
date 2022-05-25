f0fbcd51836514fcbf03816b2408d13c8f30acd8
HereticWay <vpleft@gmail.com>
2022-05-25 10:31:04 +0200

    improve BookController;

--- src/main/java/com/example/bookapimanytomany/controller/BookController.java
+++ src/main/java/com/example/bookapimanytomany/controller/BookController.java
@@ -1,17 +1,32 @@
 package com.example.bookapimanytomany.controller;
 
+import com.example.bookapimanytomany.entity.Book;
 import com.example.bookapimanytomany.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
-    private BookService bookService;
+    private final BookService bookService;
 
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> findAllBooks() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book findBookById(@PathVariable("id") Long id) {
+        return bookService.findById(id);
     }
 }
