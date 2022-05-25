6613c453af076987ece4229c42e5e621559d08d9
HereticWay <vpleft@gmail.com>
2022-05-25 09:49:29 +0200

    add data.sql and schema.sql;

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO author (name) VALUES ('Giovanni Bocacchio');
+INSERT INTO author (name) VALUES ('F. Scott Fitzgerald');
+
+INSERT INTO book (title, author_id) VALUES ('Bible', 1);
+INSERT INTO book (title, author_id) VALUES ('The Decameron', 2);
+INSERT INTO book (title, author_id) VALUES ('The Life of Dante', 2);
+INSERT INTO book (title, author_id) VALUES ('The Great Gatsby', 1);
+INSERT INTO book (title, author_id) VALUES ('The Curious Case of Benjamin Buttons', 1);
