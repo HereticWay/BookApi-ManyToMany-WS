31ed080de8abf45686061d4decba8c770c9cab67
HereticWay <vpleft@gmail.com>
2022-05-25 10:58:29 +0200

    improve data.sql;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,9 @@
-INSERT INTO author (name) VALUES
-    ('Giovanni Bocacchio'),
-    ('F. Scott Fitzgerald');
-
-INSERT INTO book (title) VALUES
-    ('Bible');
+INSERT INTO author (id, name) VALUES
+    (1, 'Giovanni Bocacchio'),
+    (2, 'F. Scott Fitzgerald');
 
 INSERT INTO book (title, author_id) VALUES
+    ('Bible', null),
     ('The Decameron', 2),
     ('The Life of Dante', 2),
     ('The Great Gatsby', 1),
