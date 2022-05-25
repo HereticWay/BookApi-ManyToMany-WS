8a06be8566df7aaa046253df14ca9d0fa4694fd3
HereticWay <vpleft@gmail.com>
2022-05-25 10:30:55 +0200

    improve AuthorController;

--- src/main/java/com/example/bookapimanytomany/controller/AuthorController.java
+++ src/main/java/com/example/bookapimanytomany/controller/AuthorController.java
@@ -1,17 +1,32 @@
 package com.example.bookapimanytomany.controller;
 
+import com.example.bookapimanytomany.entity.Author;
 import com.example.bookapimanytomany.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/author")
 public class AuthorController {
-    private AuthorService authorService;
+    private final AuthorService authorService;
 
     @Autowired
     public AuthorController(AuthorService authorService) {
         this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> findAllAuthors() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Author findAuthorById(@PathVariable("id") Long id) {
+        return authorService.findById(id);
     }
 }
