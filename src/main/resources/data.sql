4b8442f736f95a9c1972c6e4a90bceb81460acad
HereticWay <vpleft@gmail.com>
2022-05-25 10:44:38 +0200

    improve data samples;

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,12 @@
-INSERT INTO author (name) VALUES ('Giovanni Bocacchio');
-INSERT INTO author (name) VALUES ('F. Scott Fitzgerald');
+INSERT INTO author (name) VALUES
+    ('Giovanni Bocacchio'),
+    ('F. Scott Fitzgerald');
 
-INSERT INTO book (title, author_id) VALUES ('Bible', 1);
-INSERT INTO book (title, author_id) VALUES ('The Decameron', 2);
-INSERT INTO book (title, author_id) VALUES ('The Life of Dante', 2);
-INSERT INTO book (title, author_id) VALUES ('The Great Gatsby', 1);
-INSERT INTO book (title, author_id) VALUES ('The Curious Case of Benjamin Buttons', 1);
+INSERT INTO book (title) VALUES
+    ('Bible');
+
+INSERT INTO book (title, author_id) VALUES
+    ('The Decameron', 2),
+    ('The Life of Dante', 2),
+    ('The Great Gatsby', 1),
+    ('The Curious Case of Benjamin Buttons', 1);
